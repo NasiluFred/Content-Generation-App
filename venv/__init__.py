1de5d256aada56bb6803dc9fc80c73b0927c52e1
Nasilu <frednasilu@gmail.com>
2023-09-29 01:15:14 -0400

    fixing __init__

--- venv/__init__.py
+++ venv/__init__.py
@@ -1,12 +1,11 @@
 import os
-from flask import Flask
 from config import Config
 
 #Inject Flask magic
-app = Flask(__name__)
+#app = Flask(__name__)
 
 #load configurations
-app.config.from_object( Config)
+#app.config.from_object( Config)
 
 # import routing to render the pages
 
